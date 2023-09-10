f7a8fcf5dce9a4126a41ad96c5b9f0c2b3a37648
Aleksandr <a.ustiantcev@gmail.com>
2023-09-11 01:43:54 +0300

    fix: перенесена логика в пакет main

--- src/test/java/ru/ibs/practice/tests/db/service/DataBaseService.java
+++
@@ -1,8 +0,0 @@
-package ru.ibs.practice.tests.db.service;
-
-public interface DataBaseService {
-     void addProduct(String productName, String productType, int exotic);
-     void findAll();
-     void removeProduct();
-     void checkTable();
-}
